adcff1754a1241b3ae247142b03868bd3c2bb47d
Nick Retel <nick.retel@gmail.com>
2023-11-28 02:15:53 +0100

    wip

--- src/components/inputs/TextInput.vue
+++ src/components/inputs/TextInput.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup></script>
 
 <template>
-  <input class="w-full text-3xl" type="text" placeholder="Enter your name" />
+    <input class="w-full text-5xl" type="text" placeholder="Enter your name" />
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -10,13 +10,42 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, 'index.ts'),
             name: 'Livtoff UI',
-            fileName: 'liveoff-ui',
+            fileName: 'livtoff-ui',
             formats: ['es'],
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled
+            // into your library
+            external: [
+                'vue',
+                '@heroicons/vue',
+                '@headlessui/vue',
+                '@inertiajs/vue3',
+                'mitt',
+                'lodash',
+            ],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue',
+                    '@heroicons/vue': 'Heroicons',
+                    '@headlessui/vue': 'HeadlessUI-Vue',
+                    '@inertiajs/vue3': 'Inertia Vue3',
+                    mitt: 'mitt',
+                    lodash: 'lodash',
+                },
+            },
         },
     },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    css: {
+        modules: {
+            generateScopedName: '[local]',
         },
     },
 });
